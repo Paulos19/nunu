96d55659e4bb1e3dc21bfa76b3d0563cbc28ffaf
Paulos19 <paulohenrique.012araujo@gmail.com>
2026-01-29 09:12:58 -0300

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,11 @@
-// mobile/babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-worklets/plugin',
+      'react-native-reanimated/plugin' // Este deve ser SEMPRE o último
+    ],
   };
 };
